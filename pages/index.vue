02803f1deaee410d22e679f6e157a4b3bdf2500f
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2025-04-10 16:21:02 +0300

    added secondary lines and number labels

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <script setup>
 const container = ref(null);
 const spacing = ref(50);
+const lineCount = ref(2);
 
-const createLine = (x, y, className, main = false) => {
+const createLine = (x, y, className) => {
   const line = document.createElement("div");
   line.className = className;
   line.style.left = `${x}px`;
   line.style.top = `${y}px`;
-  line.style.backgroundColor = main ? "#000000" : "#cccccc";
   return line;
 };
 
@@ -18,30 +18,79 @@ onMounted(() => {
   const xAxisCenter = containerHeight / 2;
   const yAxisCenter = containerWidth / 2;
 
-  const originXAxis = createLine(0, xAxisCenter, "line-x", true);
-  const originYAxis = createLine(yAxisCenter, 0, "line-y", true);
+  console.log(containerWidth, containerHeight);
+
+  console.log(xAxisCenter, yAxisCenter);
+
+  const originXAxis = createLine(0, xAxisCenter, "line-x main-line");
+  const originYAxis = createLine(yAxisCenter, 0, "line-y main-line");
 
   container.value.appendChild(originXAxis);
   container.value.appendChild(originYAxis);
 
+  let count = lineCount.value;
   for (let i = xAxisCenter; i > 0; i -= spacing.value) {
-    const line = createLine(0, i, "line-x");
+    const lineType =
+      count % lineCount.value === 0 ? "count-line" : "filler-line";
+    const line = createLine(0, i, "line-x " + lineType);
+    if (lineType === "count-line") {
+      const text = document.createElement("span");
+      text.innerText = (count - lineCount.value) / 2;
+      text.style.position = "absolute";
+      text.style.left = xAxisCenter + "px";
+      line.appendChild(text);
+    }
     container.value.appendChild(line);
+    count++;
   }
 
+  count = lineCount.value;
   for (let i = xAxisCenter; i < containerHeight; i += spacing.value) {
-    const line = createLine(0, i, "line-x");
+    const lineType =
+      count % lineCount.value === 0 ? "count-line" : "filler-line";
+    const line = createLine(0, i, "line-x " + lineType);
+    if (lineType === "count-line" && (count - lineCount.value) / 2 !== 0) {
+      const text = document.createElement("span");
+      text.innerText = "- " + (count - lineCount.value) / 2;
+      text.style.position = "absolute";
+      text.style.left = xAxisCenter - 10 + "px";
+      line.appendChild(text);
+    }
     container.value.appendChild(line);
+    count++;
   }
 
+  count = lineCount.value;
   for (let i = yAxisCenter; i > 0; i -= spacing.value) {
-    const line = createLine(i, 0, "line-y");
+    const lineType =
+      count % lineCount.value === 0 ? "count-line" : "filler-line";
+    const line = createLine(i, 0, "line-y " + lineType);
+    if (lineType === "count-line" && (count - lineCount.value) / 2 !== 0) {
+      const text = document.createElement("span");
+      text.innerText = "- " + (count - lineCount.value) / 2;
+      text.style.position = "absolute";
+      text.style.whiteSpace = "nowrap";
+      text.style.top = yAxisCenter - 10 + "px";
+      line.appendChild(text);
+    }
     container.value.appendChild(line);
+    count++;
   }
 
+  count = lineCount.value;
   for (let i = yAxisCenter; i < containerWidth; i += spacing.value) {
-    const line = createLine(i, 0, "line-y");
+    const lineType =
+      count % lineCount.value === 0 ? "count-line" : "filler-line";
+    const line = createLine(i, 0, "line-y " + lineType);
+    if (lineType === "count-line" && (count - lineCount.value) / 2 !== 0) {
+      const text = document.createElement("span");
+      text.innerText = (count - lineCount.value) / 2;
+      text.style.position = "absolute";
+      text.style.top = yAxisCenter - 10 + "px";
+      line.appendChild(text);
+    }
     container.value.appendChild(line);
+    count++;
   }
 });
 </script>
@@ -60,13 +109,41 @@ onMounted(() => {
 .line-x {
   position: absolute;
   width: 100%;
-  height: 1px;
   transform: translateY(-50%);
 }
 .line-y {
   position: absolute;
-  width: 1px;
   height: 100%;
   transform: translateX(-50%);
+}
+
+.main-line {
+  background-color: #000000;
+}
+
+.count-line {
+  background-color: #cccccc;
+}
+
+.filler-line {
+  background-color: #e5e5e5;
+}
+
+.line-x.main-line {
+  height: 2px;
+}
+
+.line-y.main-line {
+  width: 2px;
+}
+
+.line-x.count-line,
+.line-x.filler-line {
+  height: 1px;
+}
+
+.line-y.count-line,
+.line-y.filler-line {
+  width: 1px;
 }
 </style>
