e9f59740215050e6365dda75c5fdac618209ec85
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2025-04-11 16:48:29 +0300

    added Icon package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/ui"],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxt/icon"],
   css: ["@/assets/css/main.css"],
 });
