e4b026f03fe8ca6c9dea7e27678da73c2fbfb749
Peshang Hiwa <51766800+peshanghiwa@users.noreply.github.com>
2025-04-11 17:24:55 +0300

    added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxt/icon"],
   css: ["@/assets/css/main.css"],
+  ui: {
+    colorMode: false,
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
